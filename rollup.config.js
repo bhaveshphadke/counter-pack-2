a18fff0694273d71c0e6960e938a894163a35c25
Bhavesh Phadke <bhaveshphadke5256@gmail.com>
2024-05-19 15:21:29 +0530

    initial-commit

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "rollup";
+import babel from '@rollup/plugin-babel';
+export default defineConfig({
+    input:'src/index.js',
+    output:{
+        dir:'dist',
+        format:'es',
+        name:'bhavesh-demo2'
+    },
+    external:['react','react-dom'],
+    // plugins:
+    plugins: [
+        babel({
+          presets: ['@babel/preset-react'],
+          babelHelpers: 'bundled'
+        })
+      ]
+})
