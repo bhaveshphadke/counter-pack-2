91b1f25d76a3467221218550f36f15817a09a630
Bhavesh Phadke <bhaveshphadke5256@gmail.com>
2024-06-06 21:38:49 +0530

    test

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,21 @@
 import { defineConfig } from "rollup";
 import babel from '@rollup/plugin-babel';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
 export default defineConfig({
     input:'src/index.js',
     output:{
         dir:'dist',
         format:'es',
-        name:'bhavesh-demo2'
+        name:'counter-pack-2'
     },
     external:['react','react-dom'],
-    // plugins:
     plugins: [
-        babel({
-          presets: ['@babel/preset-react'],
-          babelHelpers: 'bundled'
-        })
+      peerDepsExternal(),
+      resolve(),
+      babel({
+        presets: ['@babel/preset-react'],
+        babelHelpers: 'bundled'
+      })
       ]
 })
